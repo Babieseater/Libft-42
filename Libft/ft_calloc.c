81125c06a3c1fc153bb2bbe5a0440eddfc8bdc2a
Steven Mayrand <smayrand@c1r4p3.42quebec.com>
2022-04-20 14:05:05 -0400

    Last Push 2

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: smayrand <smayrand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 13:01:07 by smayrand          #+#    #+#             */
-/*   Updated: 2022/04/06 13:25:12 by smayrand         ###   ########.fr       */
+/*   Updated: 2022/04/20 13:56:52 by smayrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*tab;
 
+	if (count == SIZE_MAX && size == SIZE_MAX)
+		return (NULL);
 	tab = malloc(count * size);
 	if (!tab)
 		return (NULL);
